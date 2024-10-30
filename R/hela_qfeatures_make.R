8e37d6b2023bd952cc7bcc52279155ee45789e59
leopoldguyot <leopold.guyot13@gmail.com>
2024-10-30 15:02:12 +0100

    Make hela qfeatures: SE list

--- R/hela_qfeatures_make.R
+++ R/hela_qfeatures_make.R
@@ -1,28 +1,34 @@
 library(rpx)
 library(QFeatures)
+library(SummarizedExperiment)
 
 px <- PXDataset("PXD042233")
-test <- read.delim("2013_04_03_16_54_Q-Exactive-Orbitrap_1/evidence.txt")
 maxzip <- grep("Orbitrap.*zip", pxfiles(px), value = TRUE)
 sampleAnnot <- pxget(px, "Experimental-Design.sdrf.tsv")
-sampleAnnot <- read.delim(sampleAnnot)
+sampleAnnot <- DataFrame(read.delim(sampleAnnot))
 sampleAnnot$runCol <- sampleAnnot$source.name
 sampleAnnot$quantCols <- rep("Intensity", nrow(sampleAnnot))
+rownames(sampleAnnot) <- sampleAnnot$source.name
 maxzip <- maxzip[1:10]
 
-
-matrixList <- lapply(maxzip, function(x){
+SEList <- lapply(maxzip, function(x){
     zipFile <- pxget(px, x)
     baseName <- sub(".zip", "", x)
     unzip(zipFile, files = file.path(baseName, "evidence.txt"), exdir = "tmp")
     quantTable <- read.delim(file.path("tmp", baseName, "evidence.txt"))
+    quantTable$PSMid <- paste0(baseName, "_PSM", seq(nrow(quantTable)))
     quantTable$runCol <- rep(baseName, nrow(quantTable))
-    quantTable
+    rownames(quantTable) <- quantTable$PSMid
+    SE <- readSummarizedExperiment(quantTable,
+                                   quantCols = "Intensity",
+                                   fnames = "PSMid")
+    colData(SE) <- sampleAnnot[sampleAnnot$source.name == baseName, ]
+    colData(SE) <- NULL
+    SE
 })
-
 unlink("tmp", recursive = TRUE)
-quantTable <- do.call(rbind, matrixList)
 
-qfeatures <- readQFeatures(quantTable, colData = sampleAnnot, runCol = "runCol")
+names(SEList) <- sub(".zip", "", maxzip)
+qfeatures <- QFeatures(SEList, colData = sampleAnnot)
 
 saveRDS(qfeatures, file = "dataOutput/hela/qfeatures_PSM.rds")
