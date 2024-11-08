e472fc4033bc9a2f89f0eef7f8a45e64448feedf
leopoldguyot <leopold.guyot13@gmail.com>
2024-11-08 12:04:12 +0100

    update hela to QFeatures converter

--- R/get_hela_data.R
+++
@@ -1,8 +0,0 @@
-library(curl)
-library(rpx)
-library(SummarizedExperiment)
-px <- PXDataset("PXD042233")
-url <- pxurl(px)
-files <- grep("Orbitrap.*zip", pxfiles(px), value = TRUE)
-files <- files[1:10]
-sucess <- multi_download(file.path(url, files), destfiles = file.path("data/hela/rawData", files))
---
+++ R/hela_to_qfeatures.R
@@ -0,0 +1,43 @@
+library(curl)
+library(rpx)
+library(QFeatures)
+px <- PXDataset("PXD042233")
+url <- pxurl(px)
+files <- grep("Orbitrap.*zip", pxfiles(px), value = TRUE)
+files <- files[1:100]
+
+sampleAnnot <- pxget(px, "Experimental-Design.sdrf.tsv")
+sampleAnnot <- DataFrame(read.delim(sampleAnnot))
+sampleAnnot$runCol <- sampleAnnot$source.name
+sampleAnnot$quantCols <- rep("Intensity", nrow(sampleAnnot))
+rownames(sampleAnnot) <- sampleAnnot$source.name
+
+if (dir.exists("data/hela/rawData")){
+    unlink("data/hela/rawData", recursive = TRUE)
+}
+
+dir.create("data/hela/rawData")
+
+sucess <- multi_download(file.path(url, files), destfiles = file.path("data/hela/rawData", files))
+for (zip in sucess$destfile) {
+    baseName <- basename(sub(".zip", "", zip))
+    unzip(zip, files = file.path(baseName, "evidence.txt"), exdir = "data/hela/rawData")
+    file.remove(zip)
+}
+SEList <- lapply(list.files("data/hela/rawData"), function(dir){
+    fullPath <- file.path("data/hela/rawData", dir, "evidence.txt") 
+    quantTable <- read.delim(fullPath)
+    quantTable$PSMid <- paste0(dir, "_PSM", seq(nrow(quantTable)))
+    quantTable$runCol <- rep(dir, nrow(quantTable))
+    rownames(quantTable) <- quantTable$PSMid
+    SE <- readSummarizedExperiment(quantTable,
+                                   quantCols = "Intensity",
+                                   fnames = "PSMid")
+    colnames(SE) <- baseName
+    SE
+})
+
+names(SEList) <- list.files("data/hela/rawData")
+qfeatures <- QFeatures(SEList, colData = sampleAnnot)
+
+saveRDS(qfeatures, file = "dataOutput/hela/qfeatures_PSM.rds")
