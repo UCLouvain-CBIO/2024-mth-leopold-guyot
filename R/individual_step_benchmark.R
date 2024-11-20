b39ec4c2e92683c3ae79be45554bae7c38afe53e
leopoldguyot <leopold.guyot13@gmail.com>
2024-11-20 13:35:45 +0100

    benchmark individual steps v1

--- R/individual_step_benchmark.R
+++ R/individual_step_benchmark.R
@@ -11,12 +11,12 @@ benchmarkFilterFeatures <- function(qfeatures) {
     filterFeatures(qfeatures, ~ filterBench > 1)
 }
 
-benchmarkFilterSamples <- function(variables) {
-    subsetByColData(qfeatures, leduc$filterBench > 1)
+benchmarkFilterSamples <- function(qfeatures) {
+    subsetByColData(qfeatures, qfeatures$filterBench > 1)
 }
 
-benchmarkZeroisNA <- function(variables) {
-    zeroIsNA(qfeatures)
+benchmarkZeroisNA <- function(qfeatures) {
+    zeroIsNA(qfeatures, i = seq_along(qfeatures))
 }
 
 stepsPSM <- list(
@@ -31,6 +31,15 @@ stepsPro <- c()
 
 unlink(destPath, recursive = TRUE)
 dir.create(destPath)
+write.table(data.frame(nCell = integer(),
+                       rep = integer(),
+                       step = character(),
+                       sizeBefore = integer(),
+                       sizeAfter = integer()),
+            file = file.path(destPath, "qfeatures_size_report.tsv"),
+            append = FALSE,
+            col.names = TRUE,
+            row.names = FALSE)
 
 leduc <- scpdata::leduc2022_pSCoPE()
 
@@ -39,19 +48,33 @@ sizes <- c(500, 1000)
 for (i in 1:replicate) {
     for (size in sizes) {
         qfeatures <- generateTMTPSM(leduc, size)
+        qfeaturesSizeBefore <- object.size(qfeatures)
         for (stepPSM in names(stepsPSM)) {
             destFile <- file.path(destPath,
-                paste0(size, "_", stepPSM, "_", i))
-            write.csv(peakRAM(stepsPSM[[stepPSM]](qfeatures)),
+                paste0(size, "_", stepPSM, "_", i, ".csv"))
+            write.csv(peakRAM(qfeaturesAfter <- stepsPSM[[stepPSM]](qfeatures),
+                              write.table(data.frame(nCell = as.integer(size),
+                                                     rep = as.integer(i),
+                                                     step = stepPSM,
+                                                     sizeBefore = qfeaturesSizeBefore,
+                                                     sizeAfter = object.size(qfeaturesAfter)),
+                                          file = file.path(destPath, "qfeatures_size_report.tsv"),
+                                          append = TRUE,
+                                          col.names = FALSE,
+                                          row.names = FALSE)
+                              ),
                 destFile)
+
+
         }
-        qfeatures <- generateTMTPeptides(qfeatures)
-        for (stepPep in stepsPep) {
-            peakRAM(stepPep(qfeatures))
-        }
-        qfeatures  <- generateTMTProteins(qfeatures)
-        for (stepPro in stepsPro) {
-            peakRAM(stepPro(qfeatures))
-        }
+        # qfeatures <- generateTMTPeptides(qfeatures)
+        # for (stepPep in stepsPep) {
+        #     peakRAM(stepPep(qfeatures))
+        # }
+        # qfeatures  <- generateTMTProteins(qfeatures)
+        # for (stepPro in stepsPro) {
+        #     peakRAM(stepPro(qfeatures))
+        # }
+
     }
 }
