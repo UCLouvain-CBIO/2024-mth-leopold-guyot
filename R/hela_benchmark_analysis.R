2748cc0936b9f6b1d335d4cceb34acdf71ad441b
leopoldguyot <leopold.guyot13@gmail.com>
2025-02-14 10:21:57 +0100

    hela Benchmark result analysis

---
+++ R/hela_benchmark_analysis.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+
+hela <- read.table("dataOutput/helaBenchmark/size_report.tsv", header = TRUE)
+helaOpti <- read.table("dataOutput/helaBenchmark_opti/size_report.tsv", header = TRUE)
+
+df_combined <- bind_rows(
+    hela %>% mutate(type = "Vanilla"),
+    helaOpti %>% mutate(type = "Optimized")
+) %>%
+    mutate(state = factor(state, levels = c("before", "after"))) %>%
+    mutate(sizeTotal = sizeTotal / 1e9,
+           sizeAssay = sizeAssay / 1e9,
+           sizeRowData = sizeRowData / 1e9,
+           sizeColData = sizeColData / 1e9)
+
+
+ggplot(df_combined, aes(x = state, y = sizeTotal, fill = type)) +
+    geom_bar(position = "dodge", stat = "identity")
+
+df_long <- df_combined %>%
+    pivot_longer(cols = starts_with("size"),
+                 names_to = "SizeType",
+                 values_to = "SizeValue")
+
+
+# Create the plot
+ggplot(df_long, aes(x = state, y = SizeValue, fill = type)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    facet_wrap(~ SizeType, scales = "free_y") +
+    theme_minimal() +
+    labs(title = "Comparison of Different Size Metrics Before and After",
+         x = "State",
+         y = "Size Value",
+         fill = "Type")
+
+
+### Steps
+
+
+# Define the directories
+dirs <- c("dataOutput/helaBenchmark/memoryOutput/", "dataOutput/helaBenchmark_opti/memoryOutput/")
+
+# Function to read a file and add metadata columns
+read_file_with_metadata <- function(file_path, dir_name) {
+    read_csv(file_path) %>%
+        mutate(
+            Directory = dir_name,
+            Filename = basename(file_path)
+        )
+}
+df <- data.frame()
+for (dir in dirs) {
+    files <- list.files(path = dir, full.names = TRUE)
+    for (file in files) {
+        df <- rbind(df, read_file_with_metadata(file, dir))
+    }
+}
+
+# Convert numeric columns
+df <- df %>%
+    mutate(
+        Elapsed_Time_sec = as.numeric(Elapsed_Time_sec),
+        Total_RAM_Used_MiB = as.numeric(Total_RAM_Used_MiB),
+        Peak_RAM_Used_MiB = as.numeric(Peak_RAM_Used_MiB)
+    )
+df %>%
+    group_by(Filename, Directory) %>%
+    summarise(totalTime = sum(Elapsed_Time_sec)) %>%
+    ggplot(aes(x = Directory, y = totalTime)) +
+        geom_boxplot()
+
+df %>%
+    filter(Function_Call == "hela<-joining(hela,assaysNames)") %>%
+    group_by(Filename, Directory) %>%
+    ggplot(aes(x = Directory, y = Elapsed_Time_sec)) +
+    geom_boxplot()
+
+df %>%
+    filter(Function_Call == "hela<-aggregationToPep(hela,assaysNames)") %>%
+    group_by(Filename, Directory) %>%
+    ggplot(aes(x = Directory, y = Elapsed_Time_sec)) +
+    geom_boxplot()
