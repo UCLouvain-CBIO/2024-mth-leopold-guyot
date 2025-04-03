0158140c32107173ed22bb08aa1f0a9af55ecbb4
leopoldguyot <leopold.guyot13@gmail.com>
2025-04-03 11:43:36 +0200

    VerR bench with agg v2

--- R/analysisVerR.R
+++ R/analysisVerR.R
@@ -2,3 +2,26 @@ library("tidyverse")
 
 noSub <- read.csv(file = "dataOutput/optimisationsBench/noSubsetRowData.csv")
 sub <- read.csv(file = "dataOutput/optimisationsBench/subsetRowData.csv")
+
+noSub <- noSub %>%
+    separate(X, into = c("nAssay", "rep"), sep = "\\.") %>%
+    mutate(numNAssay = as.numeric(nAssay)) %>%
+    mutate(nAssay = factor(nAssay, levels = sort(unique(as.numeric(nAssay))))) %>%
+    filter(version == "base" | version == "aggregation")
+
+sub <- sub %>%
+    separate(X, into = c("nAssay", "rep"), sep = "\\.") %>%
+    mutate(numNAssay = as.numeric(nAssay)) %>%
+    mutate(nAssay = factor(nAssay, levels = sort(unique(as.numeric(nAssay))))) %>%
+    filter(version == "base" | version == "aggregation")
+
+ggplot(noSub, aes(x = numNAssay, y = Elapsed_Time_sec, colour = version)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE, aes(group = version)) +
+    labs(x = "nAssay (numeric)", y = "Elapsed Time (sec)", title = "Performance Growth for noSub")
+
+ggplot(sub, aes(x = numNAssay, y = Elapsed_Time_sec, colour = version)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE, aes(group = version)) +
+    labs(x = "nAssay (numeric)", y = "Elapsed Time (sec)", title = "Performance Growth for sub")
+
--- R/setupVerR.R
+++ R/setupVerR.R
@@ -4,6 +4,7 @@ MultiAssayExperimentBase <- "waldronlab/MultiAssayExperiment@325c85ca26582027bcd
 subsetByColDataOpti <- "waldronlab/MultiAssayExperiment@a293ab1fd1d5ac6671a4fb7a6cc91d14b21d0758"
 QFeaturesBase <- "rformassspectrometry/QFeatures@dc229a06c4feb7f61d1bc1ad7c9d626d58deb93a"
 QFeaturesAgg <- "rformassspectrometry/QFeatures@9299c7bf86b9598a4db49e04f76e999cdfdd4952"
+QFeaturesAgg2 <- "leopoldguyot/QFeatures@d7496b4f6a3e8aba2be4cf63fd8856b74eec3e41"
 scpBase <- "UCLouvain-CBIO/scp@8818589510ffd69673b533f614fc3f3eb7f9598d"
 
 envCreate("base",
@@ -23,6 +24,13 @@ envCreate("base",
 envCreate("aggregation",
           packages = c(MultiAssayExperimentBase,
                        QFeaturesAgg,
+                       scpBase,
+                       "bioc::scpdata",
+                       "peakRAM"))
+
+envCreate("aggregation2",
+          packages = c(MultiAssayExperimentBase,
+                       QFeaturesAgg2,
                        scpBase,
                        "bioc::scpdata",
                        "peakRAM"))
@@ -56,3 +64,5 @@ sub <- runInEnv({
 sub <- bind_rows(sub, .id = "version")
 
 write.csv(sub, file = "dataOutput/optimisationsBench/subsetRowData.csv")
+
+unlink(".envs", recursive = TRUE, force = TRUE)
