2f053c0476ee3ca4eb933c699fb124db6b2de938
leopoldguyot <leopold.guyot13@gmail.com>
2025-05-22 18:52:10 +0200

    rerun simu

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
 library(rmarkdown)
 
-renderRmarkdown <- function(rmdFile) {
+renderRmarkdown <- function(rmdFile, extension = ".html") {
     rmarkdown::render(file.path("Rmd", rmdFile),
-        output_file = sub(".rmd", ".html", rmdFile),
+        output_file = sub(".rmd", extension, rmdFile),
         output_dir = "reports",
         knit_root_dir = getwd()
     )
