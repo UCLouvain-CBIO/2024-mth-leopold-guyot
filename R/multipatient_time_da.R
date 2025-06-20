a9b66422c3d3877cb3e1307dd798725bc7fafc09
leopoldguyot <leopold.guyot13@gmail.com>
2025-06-20 10:35:40 +0200

    processing multipatient data

--- R/multipatient_time_da.R
+++ R/multipatient_time_da.R
@@ -11,9 +11,9 @@ coldata <- read.csv("/mnt/disk3/slavov_data/scprot_metadata.csv")
 coldata$quantCols <- coldata$sc_id
 coldata$runCols <- coldata$raw.file
 rownames(coldata) <- coldata$sc_id
-subpepColdata <- pep[, c("sc_id", "raw.file")] %>% 
-  group_by(sc_id) %>% 
-  summarize("raw.file" = unique(raw.file)) %>% 
+subpepColdata <- pep[, c("sc_id", "raw.file")] %>%
+  group_by(sc_id) %>%
+  summarize("raw.file" = unique(raw.file)) %>%
   filter(sc_id %in% coldata$sc_id)
 coldataJoined <- left_join(coldata, subpepColdata, by = "sc_id")
 rownames(coldataJoined) <- rownames(coldata)
@@ -27,11 +27,31 @@ pepse <- readSummarizedExperiment(widepep, quantCols = 2:ncol(widepep), fnames =
 coldataJoined <- coldataJoined[coldataJoined$sc_id %in% colnames(pepse), ]
 colData(pepse) <- as(coldataJoined, "DataFrame")
 pepse <- logTransform(pepse)
-colData(pepse) <- colData(pepse) %>% 
-  as.data.frame() %>% 
-  separate_wider_delim(cols = patient_id, delim = "_", names = c("patient_id", "dayp")) %>% 
+colData(pepse) <- colData(pepse) %>%
+  as.data.frame() %>%
+  separate_wider_delim(cols = patient_id, delim = "_", names = c("patient_id", "dayp")) %>%
   as("DataFrame")
 
 
 # Processing
+
+pepse <- zeroIsNA(pepse)
+
+pepse <- filterNA(pepse, pNA = 0.98)
+
+pepse <- sweep(pepse,
+      MARGIN = 2,
+      FUN = "/",
+      STATS = colMedians(assay(pepse), na.rm = TRUE))
+
+pepseMod <- scpModelWorkflow(pepse, formula = ~ 1 + patient + day + raw.file + lc_batch)
+
+dayRes <- scpDifferentialAnalysis(
+  pepseMod,
+  contrasts = list(c("day", "d0", "d"))
+)[[1]] %>%
+  filter(padj <= 0.05,
+         Estimate >= 1) %>%
+  rownames()
+
 
