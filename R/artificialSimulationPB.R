0d8e32a6003f01f63dbffcf037efcbe42d0812f3
leopoldguyot <leopold.guyot13@gmail.com>
2025-07-14 12:01:36 +0200

    explore artificial sim results

--- R/artificialSimulationPB.R
+++ R/artificialSimulationPB.R
@@ -28,17 +28,17 @@ compute_performance <- function(modRes, rowdata) {
     select(sid, is_da) %>%
     distinct() %>%
     column_to_rownames("sid")
-  
+
   adjPval_df <- df %>%
     select(sid, method, adjPval) %>%
     pivot_wider(names_from = method, values_from = adjPval) %>%
     column_to_rownames("sid")
-  
+
   cobdata <- COBRAData(
     padj = as.data.frame(adjPval_df),
     truth = as.data.frame(truth_df)
   )
-  
+
   perf <- calculate_performance(cobdata,
                                 binary_truth = "is_da",
                                 aspects = "fdrtpr",
@@ -62,65 +62,65 @@ simulateCellPatientData <- function(expMetrics, rowdata,
   residualSD <- expMetrics$sdIntensity
   missingProb <- expMetrics$pctMissing/100
   nProt <- length(mu)
-  
+
   nCellTot <- nPatient * nPopulation * nCellPatPop
-  
+
   # Initialize matrix
   simMatrix <- matrix(NA, nrow = nProt, ncol = nCellTot)
   rownames(simMatrix) <- proteinNames
-  
+
   nPatientNames <- paste0("Patient", 1:nPatient)
   nPopulationNames <- paste0("CellType", 1:nPopulation)
   nCellPatPop <- 1:nCellPatPop
-  
+
   dfCellId <- expand.grid(
     Patient = nPatientNames,
     CellType = nPopulationNames,
     Cell = nCellPatPop
-  )  
+  )
   cellIds <- with(dfCellId, paste(Patient, CellType, Cell, sep = "_"))
   colnames(simMatrix) <- cellIds
 
   # Number of proteins to shift
   nPatientShiftProt <- floor(patientEffect * nProt)
   nPopulationShiftProt <- floor(populationEffect * nProt)
-  
+
   # Select proteins affected by patient and population effect
   patientShiftProteins <- sample(proteinNames, nPatientShiftProt)
   populationShiftProteins <- sample(proteinNames, nPopulationShiftProt)
-  
+
   for (i in 1:nProt) {
     simMatrix[i, ] <- rnorm(nCellTot, mean = mu[i], sd = residualSD[i])
   }
-  
+
   # Apply patient effect shifts
   for (prot in patientShiftProteins) {
     protIdx <- which(proteinNames == prot)
-    
+
     for (pat in nPatientNames) {
-      shiftVal <- rnorm(1, 
-                        mean = patientShift * mu[protIdx], 
+      shiftVal <- rnorm(1,
+                        mean = patientShift * mu[protIdx],
                         sd = patientSD * mu[protIdx])
-      
+
       cols <- grep(paste0("^", pat, "_"), colnames(simMatrix))
       simMatrix[protIdx, cols] <- simMatrix[protIdx, cols] + shiftVal
     }
   }
-  
+
   # Apply population (cell type) effect shifts
   for (prot in populationShiftProteins) {
     protIdx <- which(proteinNames == prot)
-    
+
     for (pop in nPopulationNames) {
-      shiftVal <- rnorm(1, 
-                        mean = populationShift * mu[protIdx], 
+      shiftVal <- rnorm(1,
+                        mean = populationShift * mu[protIdx],
                         sd = populationSD * mu[protIdx])
-      
+
       cols <- grep(paste0("_", pop, "_"), colnames(simMatrix))
       simMatrix[protIdx, cols] <- simMatrix[protIdx, cols] + shiftVal
     }
   }
-  
+
   # Add missingness at random
   for (p in seq_len(nProt)) {
     missing <- runif(nCellTot) < missingProb[p]
@@ -138,47 +138,47 @@ addTreatmentEffect <- function(sce, expMetrics,
                                treatmentSD,
                                seed = NULL) {
   if (!is.null(seed)) set.seed(seed)
-  
+
   mu <- expMetrics$meanIntensity
   proteinNames <- expMetrics$protNames
   nProt <- length(mu)
-  
+
   nTreatmentShiftProt <- floor(treatmentEffect * nProt)
-  
+
   treatmentShiftProteins <- sample(proteinNames, nTreatmentShiftProt)
-  
+
   # Pick half the patients (randomly)
   patientLevels <- unique(colData(sce)$Patient)
   nPatients <- length(patientLevels)
   nTreated <- floor(nPatients / 2)
-  
+
   treatedPatients <- sample(patientLevels, nTreated)
-  
+
   assayMatrix <- assay(sce)
-  
+
   # Apply shifts
   for (prot in treatmentShiftProteins) {
     protIdx <- which(rownames(assayMatrix) == prot)
-    
+
     for (pat in treatedPatients) {
-      shiftVal <- rnorm(1, 
+      shiftVal <- rnorm(1,
                         mean = treatmentShift * mu[protIdx],
                         sd = treatmentSD * mu[protIdx])
-      
+
       cols <- which(colData(sce)$Patient == pat)
-      
+
       assayMatrix[protIdx, cols] <- assayMatrix[protIdx, cols] + shiftVal
     }
   }
-  
+
   # Save treated/untreated status in colData
   colData(sce)$Treatment <- ifelse(colData(sce)$Patient %in% treatedPatients,
                                    "Treated", "Control")
   # Add logical column for shifted proteins
   rowData(sce)$TreatmentShiftedProtein <- rownames(sce) %in% treatmentShiftProteins
-  
+
   assay(sce) <- assayMatrix
-  
+
   return(sce)
 }
 
@@ -189,7 +189,7 @@ customRobustSummary <- function(x, ...) {
     fit <- MASS::rlm(row ~ 1, ...)
     coef(fit)[1]
   })
-  
+
   names(res) <- rownames(x)
   res
 }
@@ -202,7 +202,7 @@ aggregate_se <- function(se, group_by_cols, fun = mean, robustSummary = FALSE) {
   colData(se)$Group <- apply(colData(se)[, group_by_cols, drop = FALSE], 1, paste, collapse = "_")
   group_factor <- factor(colData(se)$Group)
   assay_matrix <- assay(se)
-  
+
   # Aggregate assay matrix by group_factor levels order
   grouped_indices <- split(seq_along(group_factor), group_factor)
   levels_group <- levels(group_factor)  # factor levels in order
@@ -236,13 +236,13 @@ benchmarkMethods <- function(expMetrics, rowdata,
                              populationEffect, populationShift, populationSD,
                              treatmentEffect, treatmentShift, treatmentSD,
                              seed = NULL) {
-  
+
   simSCE <- simulateCellPatientData(expMetrics = protMetrics, rowdata = rowdata,
                           nPatient = nPatient, nPopulation = nPopulation,  nCellPatPop = nCellPatPop,
                           patientEffect = patientEffect, patientShift = patientShift, patientSD = patientSD,
                           populationEffect = populationEffect, populationShift = populationShift, populationSD = populationSD,
                           seed = seed)
-  
+
   simSCE <- addTreatmentEffect(simSCE, expMetrics = protMetrics,
                                treatmentEffect = treatmentEffect, treatmentShift = treatmentShift, treatmentSD = treatmentSD, seed = seed)
 
@@ -256,26 +256,26 @@ benchmarkMethods <- function(expMetrics, rowdata,
     scpModel,
     contrasts = list(c("Treatment", "Control", "Treated"))
   )[[1]]
-  
+
   colnames(scpRes) <- c("feature", "logFC", "se", "df", "t", "pval", "adjPval")
   rownames(scpRes) <- scpRes$feature
-  
+
   L <- makeContrast("TreatmentTreated=0", parameterNames = c("TreatmentTreated"))
   msqModel <- suppressMessages(suppressWarnings(msqrob(simSCE, formula = ~ CellType + Treatment + (1 | Patient))))
   msqRes <- rowData(hypothesisTest(object = msqModel, contrast = L))$TreatmentTreated
-  
-  aggMeanMod <- suppressWarnings(msqrob(aggMean, ~ 1 + Treatment + CellType))
+
+  aggMeanMod <- suppressWarnings(msqrob(aggMean, ~ 1 + Treatment + CellType + (1 | Patient)))
   aggMeanRes <- rowData(hypothesisTest(object = aggMeanMod, contrast = L))$TreatmentTreated
-  
-  aggMedianMod <- suppressWarnings(msqrob(aggMedian, ~ 1 + Treatment + CellType))
+
+  aggMedianMod <- suppressWarnings(msqrob(aggMedian, ~ 1 + Treatment + CellType + (1 | Patient)))
   aggMedianRes <- rowData(hypothesisTest(object = aggMedianMod, contrast = L))$TreatmentTreated
-  
-  aggSumMod <- suppressWarnings(msqrob(aggSum, ~ 1 + Treatment + CellType))
+
+  aggSumMod <- suppressWarnings(msqrob(aggSum, ~ 1 + Treatment + CellType + (1 | Patient)))
   aggSumRes <- rowData(hypothesisTest(object = aggSumMod, contrast = L))$TreatmentTreated
-  
-  aggRobustMod <- suppressWarnings(msqrob(aggRobust, ~ 1 + Treatment + CellType))
+
+  aggRobustMod <- suppressWarnings(msqrob(aggRobust, ~ 1 + Treatment + CellType + (1 | Patient)))
   aggRobustRes <- rowData(hypothesisTest(object = aggRobustMod, contrast = L))$TreatmentTreated
-  
+
   fdrtpr <- compute_performance(list("scp" = scpRes,
                                      "msqrob2" = msqRes,
                                      "pseudobulkMean" = aggMeanRes,
@@ -283,7 +283,7 @@ benchmarkMethods <- function(expMetrics, rowdata,
                                      "pseudobulkSum" = aggSumRes,
                                      "pseudobulkRobustSummary" = aggRobustRes
                                      ), rowdata = rowData(simSCE))
-  
+
 }
 
 base <- scpdata::brunner2022()
@@ -309,15 +309,15 @@ protMetrics <- data.frame(
 
 benchRes <- list()
 
-for (nCellPatPop in c(10, 25, 50)) {
-  for (treatmentShift in c(0.1, 0.7)) {
+for (nCellPatPop in c(10, 25, 50, 100)) {
+  for (treatmentShift in c(0.1, 0.3, 0.5, 1)) {
     cat("Starting simulation:", "nCell = ", nCellPatPop, ", shift = ", treatmentShift, "\n")
-    benchRes[[paste0("nCell", nCellPatPop, "_", "shift", treatmentShift)]] <- 
+    benchRes[[paste0("nCell", nCellPatPop, "_", "shift", treatmentShift)]] <-
       benchmarkMethods(protMetrics, rowdata = rowData(sce),
                      nPatient = 16, nPopulation = 5,  nCellPatPop = nCellPatPop,
-                     patientEffect = 1, patientShift = 0.3, patientSD = 0.1,
-                     populationEffect = 0.3, populationShift = 0.3, populationSD = 0.1,
-                     treatmentEffect = 0.3, treatmentShift = treatmentShift, treatmentSD = treatmentShift/4,
+                     patientEffect = 1, patientShift = 0.2, patientSD = 0.05,
+                     populationEffect = 0.33, populationShift = 0.4, populationSD = 0.1,
+                     treatmentEffect = 0.33, treatmentShift = treatmentShift, treatmentSD = treatmentShift/4,
                      seed = 123)
   }
 }
---
+++ R/artificialSimulationPBResults.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+
+res <- readRDS("dataOutput/artificialSimPB/tprfdrRes.rds")
+
+for (name in names(res)) {
+    splited <- str_split_1(name, "_")
+    res[[name]]$parameters <- name
+    res[[name]]$cellPerComb <- splited[[1]]
+    res[[name]]$introducedShift <- splited[[2]]
+}
+
+combined <- do.call(rbind, res)
+
+## Per cell per combination
+
+combined %>%
+    filter(introducedShift == "shift0.7") %>%
+    ggplot(aes(x = FDR, y = TPR, color = cellPerComb)) +
+    geom_vline(
+        xintercept = c(0.01, 0.05, 0.1),
+        linetype = "dashed", color = "grey50", linewidth = 0.3
+    ) +
+    geom_point(size = 2.5, alpha = 0.8) +
+    geom_line(size = 0.7) +
+    scale_x_continuous(
+        trans = "sqrt",
+        limits = c(0, 1),
+        breaks = c(0.01, 0.2, 0.6, 1),
+        labels = scales::label_number()
+    ) +
+    scale_y_continuous(
+        limits = c(0, 1),
+        breaks = seq(0, 1, 0.2)
+    ) +
+    theme_minimal() +
+    labs(
+        x = "False Discovery Rate (sqrt scale)",
+        y = "True Positive Rate",
+        color = "Number of Cells per Combination"
+    ) + facet_wrap(~method) +
+    theme(
+        panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+        legend.position = "bottom"
+    )
+
+ggsave("Figs/artiSimPerCell.pdf")
+
+## Per shift plot
+combined %>%
+    filter(cellPerComb == "nCell50") %>%
+    ggplot(aes(x = FDR, y = TPR, color = method)) +
+        geom_vline(
+            xintercept = c(0.01, 0.05, 0.1),
+            linetype = "dashed", color = "grey50", linewidth = 0.3
+        ) +
+        geom_point(size = 2.5, alpha = 0.8) +
+        geom_line(size = 0.7) +
+        scale_x_continuous(
+            trans = "sqrt",
+            limits = c(0, 1),
+            breaks = c(0.01, 0.2, 0.6, 1),
+            labels = scales::label_number()
+        ) +
+        scale_y_continuous(
+            limits = c(0, 1),
+            breaks = seq(0, 1, 0.2)
+        ) +
+        theme_minimal() +
+        labs(
+            x = "False Discovery Rate (sqrt scale)",
+            y = "True Positive Rate",
+            color = "Method"
+        ) +
+        facet_wrap(~introducedShift) +
+        theme(
+            panel.border = element_rect(color = "black", fill = NA, size = 0.5),
+            legend.position = "bottom"
+        )
+
+ggsave("Figs/artiSimPerShift.pdf")
