99e1cfec6f4ce4382541e3e2956cc28cdae42f57
leopoldguyot <leopold.guyot13@gmail.com>
2025-09-02 16:10:14 +0200

    Revert "[POST-submission] - simple opti summary plot"
    This reverts commit 2af452683525ee010ebcf2889cdf95171e180caf.

--- R/artificialSimulationPBResults.R
+++ R/artificialSimulationPBResults.R
@@ -26,8 +26,7 @@ combined_summary <- combined %>%
     )
 
 combined_summary %>%
-    filter(introducedShift == "shift0.15",
-           method ==) %>%
+    filter(introducedShift == "shift0.15") %>%
     ggplot(aes(x = FDR_mean, y = TPR_mean, color = cellPerComb)) +
     geom_vline(
         xintercept = c(0.01, 0.05, 0.1),
